64621271b0cd03a55a65fbc1b20995a03009b335
vigneshwaranc <vigneshwaranc@wekancode.com>
2020-04-05 19:59:42 +0530

    sorted the languages list

--- src/components/words.js
+++ src/components/words.js
@@ -1,22 +1,22 @@
 var PROGRAMING_LANG = [
-	"python",
-	"javascript",
-	"dart",
-	"typescript",
-	"java",
-	"html",
-	"css",
-	"c",
-	"csharp",
-	"golang",
-	"kotlin",
-	"php",
-	"sql",
-	"ruby"
+  "c",
+  "csharp",
+  "css",
+  "dart",
+  "golang",
+  "html",
+  "java",
+  "javascript",
+  "kotlin",
+  "php",
+  "python",
+  "ruby",
+  "sql",
+  "typescript",
 ];
 
 function randomWord() {
-	return PROGRAMING_LANG[Math.floor(Math.random() * PROGRAMING_LANG.length)];
+  return PROGRAMING_LANG[Math.floor(Math.random() * PROGRAMING_LANG.length)];
 }
 
 export { randomWord };
